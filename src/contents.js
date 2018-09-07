39538abae082428487c2cbba99bfa50939c94125
pluulove <pluulove@pluuloveui-MacBook-Pro.local>
2018-09-07 23:51:17 +0900

    Preview Commit

--- src/contents.js
+++ src/contents.js
@@ -1,29 +1,17 @@
 module.exports = {
   profile: {
-    name: '김우현',
-    description: 'Front-end developer',
-    dateOfBirth: '1996.11.05',
-    email: 'dngus242@gmail.com',
+    name: '노현석',
+    description: 'Android Developer',
+    email: 'pluulove@gmail.com',
     links: [
-      {name: 'github', href: 'https://github.com/KimWooHyun'},
-      {name: 'portfolio', href: 'https://kimwoohyun.github.io/portfolio/'}
+      {name: 'github', href: 'https://github.com/Pluu'},
+      {name: 'blog', href: 'http://pluu.github.io/'},
+      {name: 'portfolio', href: 'http://pluu.github.io/portfolio/'}
     ]
   },
   introduction: `
-    2014년부터 Front-End 개발을 하고 있는 김우현입니다.<br />
-    마크업과 Vue.js를 이용한 Front-End 개발에 관해 관심이 많으며 최근에는 flex와 크로스 브라우징, 반응형 웹과 사용자 인터랙션에도 관심을 두고 있습니다.<br />
-    주로 사용하는 언어는 HTML, CSS, JavaScript 입니다.<br /><br />
-    최근에 Vue.js에 관심이 생겨 사내 도입을 시도하였고 Front-End 프레임워크에 관심을 가지게 되었습니다.<br />
-    이후 리뉴얼 프로젝트에서는 Vue.js의 SSR 프레임 워크인 Nuxt.js를 이용한 서비스를 제작하여 SPA 와 SSR 개발 경험을 하였습니다.<br />
-    Nuxt.js 프로젝트에서는 보다 시멘틱 한 웹에 중점을 두었으며, CSS 프레임 워크를 따로 두지 않고 CSS 전처리 도구인 LESS 로 개발하였습니다.<br /><br />
-    디자이너와는 스케치와 제플린을 활용하여 커뮤니케이션을 하였으며 결과물을 같이 확인 하며 실시간으로 반영하고 의견 조율을 하였습니다.<br /><br />
-    오픈소스에도 관심이 많습니다.<br />
-    처음에는 Nuxt.js 의 도큐먼트 오탈자와 예제 코드 수정 등 작은 이슈들을 처리하였고, github에 있는 여러 컴포넌트 라이브러리들을 둘러보면서 오픈소스에 대한 관심이 생겼습니다.<br />
-    그러다 최근에 사내에서 쓰고 있는 기능을 구현하는 도중, 라이브러리로 만들어 배포하면 어떨까 생각하게 되어 오픈소스로 배포하였습니다.<br />
-    이후 여러 나라의 사람들과 다양한 언어로 github을 통해 같이 살을 붙여나가고 있습니다.<br />
-    현재 vue-lunar-calendar  라는 한국 음력 달력 라이브러리로 배포되었으며 총 7명의 contributors, 6개의 언어로 되어있습니다.
-    `
-  ,
+    프루입니다.<br />
+    `  ,
   contents: {
     'Skill Set': [
       {
@@ -33,312 +21,118 @@ module.exports = {
       },
       {
         'date': 'Language',
-        'title': 'HTML5 / CSS3 / JavaScript / Swift / Python',
-        'contents': ['']
-      },
-      {
-        'date': 'Framework',
-        'title': 'Vue.js / Nuxt.js / React / Django / Flask / Bootstrap',
+        'title': 'Java / Kotlin',
         'contents': ['']
       },
       {
         'date': 'Library',
-        'title': 'Webpack / jQuery / Less / Axios / Alamofire',
+        'title': 'Retrofit / OkHttp / Glide',
         'contents': ['']
-      },
-      {
-        'date': 'Dependency Manager',
-        'title': 'cocoapods / npm / yarn / pip',
-        'contents': ['']
-      }
-    ],
-    '개인 프로젝트': [
-      {
-        'date': '2017.07 - 현재',
-        'title': 'vue-lunar-calendar',
-        'contents': [
-          '- Vue.js component library',
-          '- <a href="https://github.com/KimWooHyun/vue-lunar-calendar">https://github.com/KimWooHyun/vue-lunar-calendar</a>',
-          '- 한국 음력 달력 라이브러리',
-          '- opensource',
-          '- 총 7명의 contributors, 6개의 언어'
-        ]
-      },
-      {
-        'date': '2017.08',
-        'title': 'WHLunarCalendar',
-        'contents': [
-          '- Swift library',
-          '- <a href="https://github.com/KimWooHyun/WHLunarCalendar">https://github.com/KimWooHyun/WHLunarCalendar</a>',
-          '- 한국 음력 달력 라이브러리',
-          '- opensource'
-        ]
       }
     ],
     '개인 활동': [
       {
-        'date': '2018.02 - 현재',
-        'title': 'Women Techmakers KR 2018',
-        'img': '/pa/wtm-logo.png',
+        'date': '2017.04 - 현재',
+        'title': 'GDG Korea',
         'contents': [
-          '<strong>Women Techmakers KR 2018 오거나이저</strong>',
-          '- 행사준비',
-          '- 안내사이트 제작',
-          '- <a href="https://wtm-korea-2018.firebaseapp.com/">https://wtm-korea-2018.firebaseapp.com/</a>'
+          '<strong>GDG Korea Android 오거나이저</strong>'
         ]
       },
       {
-        'date': '2017.08 - 현재',
-        'title': 'DjangoCon KR 2018',
-        'img': '/pa/djangocon-logo.png',
+        'date': '2018.09',
+        'title': '9XD & GDG Seoul 해커톤',
         'contents': [
-          '<strong>DjangoCon KR 2018 오거나이저</strong>',
-          '- 행사준비'
+          '9XD & GDG Seoul 해커톤 스태프',
+          '- 행사 준비 및 당일 행사 참여'
         ]
       },
       {
-        'date': '2017.12',
-        'title': 'Do it! Vue.js',
+        'date': '2016.1~2분기',
+        'title': '핵심만 골라 배우는 안드로이드 스튜디오 & 프로그래밍',
         'contents': [
-          'vue.js 관련 책 기술 검토 및 자문',
-          '서명: Do it! Vue.js 입문서',
-          '저자: 장기효',
-          '대상 독자: 프레임워크를 다뤄보지 않은 웹 개발 입문자, Vue.js를 배우고 싶은 대학생 & 퍼블리셔 & 실무자',
-          '책 내용: 프론트엔드 개발 프레임워크인 Vue.js 소개, 뷰 기술요소 학습, 뷰를 이용한 실전 애플리케이션 제작'
-        ]
-      },
-      {
-        'date': '2017.12',
-        'title': 'Vue.js Korea Meetup3',
-        'contents': [
-          'Vue.js Korea 3번째 밋업 스태프 및 행사 총괄'
-        ]
-      },
-      {
-        'date': '2017.11',
-        'title': 'GDG DevFest',
-        'contents': [
-          'GDG DevFest 스태프',
-          '- 안내사이트 제작 참여',
-          '- <a href="https://devfest17-seoul.firebaseapp.com/">https://devfest17-seoul.firebaseapp.com/</a>'
-        ]
-      },
-      {
-        'date': '2017.09',
-        'title': '9XD 해커톤',
-        'contents': [
-          '9XD 해커톤 "내꺼만들기, 내꺼톤" 참가',
-          '- Vue.js 전파',
-          '- 팀 주제: 불만',
-          '- 본인의 불만을 말하고 피드백을 받을 수 있는 게시판 형태의 웹 서비스 "불만투고" 제작 및 배포'
-        ]
-      },
-      {
-        'date': '2017.08',
-        'title': 'GDG Web Tech',
-        'contents': [
-          'GDG Web Tech 발표',
-          '- Vue.js 관련 발표',
-          '- <a href="https://www.slideshare.net/ssuserd3aacb/vue-js-79169830">https://www.slideshare.net/ssuserd3aacb/vue-js-79169830</a>'
-        ]
-      },
-      {
-        'date': '2017.08',
-        'title': 'Vue.js Korea Meetup2',
-        'contents': [
-          'vue.js Korea 2번째 밋업 스태프'
-        ]
-      },
-      {
-        'date': '2017.07',
-        'title': 'GDG Korea Campus summer party',
-        'contents': [
-          'GDG Korea Campus 스태프',
-          '- 행사 준비',
-          '- 안내사이트 제작 참여',
-          '- <a href="https://gdgsummerparty.firebaseapp.com">https://gdgsummerparty.firebaseapp.com</a>'
-        ]
+          'Android 책 검토 및 리뷰',
+          '닐 스미스 저 / 심재철 역',
+          '출판사 : 제이펍'
+        ]        
       }
     ],
-    '학력 사항': [
+    '개인 프로젝트': [
       {
-        'date': '2016 - 현재',
-        'title': '한국방송통신대학교',
-        'contents': ['컴퓨터과학과 재학중']
-      },
-      {
-        'date': '2012 - 2015',
-        'title': '미림여자정보과학고등학교 (미림 마이스터고)',
-        'contents': ['인터랙티브 미디어과 졸업']
+        'date': '2015.10 - 현재',
+        'title': 'WebToon Viewer Application',
+        'contents': [
+          '- 웹툰 Application',
+          '- Link : <a href="https://github.com/Pluu/WebToon">https://github.com/Pluu/WebToon</a>',
+          '- 다양한 웹툰 사이트의 내용을 하나의 앱내에서 보기 위해서 만든 토이 프로젝트',
+          '- 네이버, 다음, KTOON, 카카오 페이지, 네이트, One Store 지원 (일부 기능 미지원)',
+          '- 크롤링, API 요청 등을 이용하여 정보 처리',
+          '- Kotlin, AndroidX 지원',
+          '- Koin, Jsoup, Glide, OkHttp, RxJava, RxAdnroid'
+        ]
       }
     ]
   },
   summary: {
     '경력 요약': [
       {
-        'date': ['2018.04', '현재'],
-        'title': 'Spoqa (스포카)',
+        'date': ['2016.05', '현재'],
+        'title': '야놀자',
         'contents': [
-          '주 - Front-end 개발'
+          'Android 서비스 및 아키텍쳐 개발'
         ]
       },
       {
-        'date': ['2016.01', '2018.01'],
-        'title': 'ONE MOMENT (원모먼트)',
+        'date': ['2015.05', '2016.03'],
+        'title': 'EDLSolution',
         'contents': [
-          '주 - Front-end 개발',
-          '서브 - Swift, Django 개발'
+          'Android 서비스 개발'
         ]
       },
       {
-        'date': ['2014.12', '2016.01'],
-        'title': 'ATEAM VENTURES (에이팀벤쳐스)',
+        'date': ['2014.04', '2015.04'],
+        'title': '유니크인',
         'contents': [
-          '주 - Front-end 개발',
-          '서브 - Django, Flask 개발'
+          'Android 서비스 개발'
+        ]
+      },
+      {
+        'date': ['2011.11', '2014.04'],
+        'title': '(주) Hubilon',
+        'contents': [
+          'Android 서비스 개발'
+        ]
+      },
+      {
+        'date': ['2009.11', '2011.09'],
+        'title': '일본 TOMAMO',
+        'contents': [
+          'Android 서비스 개발'
         ]
       }
     ]
   },
   detailContents: {
-    'ONE MOMENT (원모먼트)': {
-      'date': '2016.01 - 2018.01',
-      'info': '90분 당일 꽃배달 서비스 “원모먼트 플라워” 를 서비스 중인 스타트업.',
+    '야놀자': {
+      'date': '2016.05 - 현재',
+      'info': '좋은 숙박을 연구하고 대한민국 숙박산업을 선도합니다.',
       'service': [
-        'Web<br /> - <a href="https://1moment.co.kr/">https://1moment.co.kr/</a> <br /> - 현재 Vue.js (SPA)로 개발된 사이트<br /><br />',
-        'iOS App<br /> - <a href="https://goo.gl/jP2ZYR">https://goo.gl/jP2ZYR</a>'
+        'Android App<br />',
+        '- <a href="https://play.google.com/store/apps/details?id=com.cultsotry.yanolja.nativeapp">야놀자 Android 어플리케이션</a> <br />'
       ],
       'project': {
         'project': [
           {
-            'date': '2017.10 - 2018.01',
-            'title': 'Nuxt.js 도입 및 SSR 지원',
+            'date': '2017.01 - 현재',
+            'title': '야놀자 서비스 개발',
             'contents': [
-              '원모먼트 서비스 사이트 전체 리뉴얼',
-              `
-              - 작업 리스트 : 
-              메인 / 상품 리스트 / 상품 상세 / 상품 리뷰  / 카테고리 / 정기구독 리스트 /  정기구독 등록 / 주문 / 주문 확인 / 마이페이지 - 기념일 / 마이페이지 - 주문리스트 / 마이페이지 - 주문상세 / 마이페이지 - 프로필 / 마이페이지 - 프로필 수정 / 적립금  / 마이페이지 - 간편결제 카드 관리 / 마이페이지 - 주소록 리스트 / 마이페이지 - 주소록 / FAQ / 이용약관 / 채용 / 회사소개 / 기획전
-              `,
-              '- Nuxt.js 도입 및 SSR 지원',
-              '- css 프레임워크 제거 및 LESS 도입',
-              '- 그리드 시스템 개발',
-              '- 시멘틱 웹 표준 준수',
-              '- axios 를 활용한 api call',
-              '- PM2, ecosystem을 이용한 배포 자동화',
-              '- 기여도 : 100%, 단독진행',
-              'stack: Vue.js, Nuxt.js, Less'
+              
             ]
           },
           {
-            'date': '2017.02 - 2017.03',
-            'title': 'Vue.js 도입',
+            'date': '2016.05 - 2018.04',
+            'title': '바로예약 서비스 개발',
             'contents': [
-              '원모먼트 서비스 사이트 전체 리뉴얼',
-              `
-              - 작업 리스트 : 
-              메인 / 상품 리스트 / 상품 상세 / 상품 리뷰  / 카테고리 / 정기구독 리스트 /  정기구독 등록 / 적립금 / FAQ / 이용약관 / 채용 / 회사소개 / 기획전
-              `,
-              '- Vue.js (SPA) 도입',
-              '- 비동기 처리를 이용한 서비스 속도 개선',
-              '- vue-resource 를 활용한 api call',
-              '- 기여도 : 45%',
-              'stack: Vue.js, Bootstrap 4, Bootstrap 3, jQuery'
-            ]
-          },
-          {
-            'date': '2016.06 - 2017.11',
-            'title': 'iOS App 런칭',
-            'contents': [
-              'iOS App 런칭',
-              `
-              - 작업 리스트 : 
-              메인 / 상품 리스트 / 상품 상세 / 상품 리뷰  / 카테고리 / 주문 / 주문 확인 / 마이페이지 - 주문리스트 / 마이페이지 - 주문상세 / 마이페이지 - 프로필 / 마이페이지 - 프로필 수정 / 마이페이지 - 주소록 리스트 / 마이페이지 - 주소록 / FAQ / 이용약관 / 채용 / 회사소개 / 기획전
-              `,
-              '- Auto Layout을 이용한 반응형 앱 개발',
-              '- iTunes Connect 배포 및 버전 관리',
-              '- cocoapods 를 활용한 라이브러리 디펜던시 관리',
-              '- alamofire 를 활용한 api call',
-              '- 기여도 : 100%, 단독진행',
-              'stack: Swift, cocoapods'
-            ]
-          },
-          {
-            'date': '2016.01 - 2016.03',
-            'title': '관리자 페이지',
-            'contents': [
-              '원모먼트 서비스 관리자 페이지 제작 및 유지보수',
-              `
-              - 작업 리스트 : 
-              퀵사 연동 / 주문 내역 (운영팀/플라워팀) / 퀵 주문 내역 / 주문 리포트 / 기념일 리포트 / 기획전 생성 페이지
-              `,
-              '- Django template을 이용한 front-end 개발',
-              '- 기여도 : 30%',
-              'stack: Django'
-            ]
-          },
-          {
-            'date': '2016.01 - 2016.03',
-            'title': '웹 서비스 런칭',
-            'contents': [
-              '원모먼트 서비스 런칭',
-              `
-              - 작업 리스트 : 
-              메인 / 상품 리스트 / 상품 상세 / 상품 리뷰  / 카테고리 / 주문 / 주문 확인 / 마이페이지 - 주문리스트 / 마이페이지 - 주문상세 / 마이페이지 - 프로필 / 마이페이지 - 프로필 수정 / 마이페이지 - 주소록 리스트 / 마이페이지 - 주소록 / FAQ / 이용약관 / 회사소개
-              `,
-              '- Django template을 이용한 front-end 개발',
-              '- 기여도 : 100%, 단독진행',
-              'stack: HTML5, CSS3, JavaScript, jQuery, Django'
-            ]
-          }
-        ]
-      }
-    },
-    'ATEAM VENTURES (에이팀벤쳐스)': {
-      'date': '2014.12 - 2016.01',
-      'info': '3D Printer 공유 플랫폼 사이트 “ShapEngine” 을 서비스 중인 스타트업.',
-      'service': [
-        `Web<br />
-        <span style="font-weight:bold">shapEngine</span><br />
-        - 3D Printer를 보유한 사람과 3D Modeling 도면을 보유한 사람을 매칭해 주는 중개 플랫폼<br />
-        - <a href="https://shapengine.com/">https://shapengine.com/</a><br /><br />
-        <span style="font-weight:bold">creatable labs</span><br />
-        - 자사 3D Printer 인 creatable 시리즈 판매 사이트<br />
-        - <a href="https://creatablelabs.com/">https://creatablelabs.com/</a>`
-      ],
-      'project': {
-        'project': [
-          {
-            'date': '2015.07 - 2016.01',
-            'title': 'shapEngine 웹 서비스 리뉴얼',
-            'contents': [
-              'shapEngine 웹 서비스 리뉴얼',
-              '- 작업 리스트 : 메인 / 프린터 등록 / 채팅 ',
-              '- Django로 리팩토링',
-              '- 기여도 : 25%',
-              'stack: Django, Jinja2, HTML5, CSS3, JavaScript'
-            ]
-          },
-          {
-            'date': '2015.06 - 2015.06',
-            'title': 'creatable labs 웹 서비스 제작',
-            'contents': [
-              'creatable labs 웹 서비스 제작',
-              '- 작업 리스트 : 블로그 페이지 제작',
-              '- 워드프레스로 제작',
-              '- 기여도 : 10%',
-              'stack: HTML5, CSS3'
-            ]
-          },
-          {
-            'date': '2014.12 - 2015.06',
-            'title': 'shapEngine 웹 서비스 제작',
-            'contents': [
-              'shapEngine 웹 서비스 제작',
-              '- 작업 리스트 : 메인 / 프린터 등록 / 채팅',
-              '- 3D Printer 를 보유 한 사람이 프린터를 등록 할 수 있는 프로세스 개발',
-              '- Jinja2을 이용한 front-end 개발',
-              '- 기여도 : 25%',
-              'stack: Flask, Jinja2, HTML5, CSS3, JavaScript'
+              
             ]
           }
         ]
